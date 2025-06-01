74ceae22008d73d3823943ac3bf426f6fac92b30
salesmiguelz <miguelsales02@gmail.com>
2025-06-01 11:34:08 -0300

    feat: checkout components

---
+++ src/pages/Checkout/index.tsx
@@ -0,0 +1,91 @@
+import { CurrencyDollarIcon, MapPinAreaIcon } from "@phosphor-icons/react";
+import { ConfirmOrderButton, Container, DeliveryAddress, DeliveryAddressHeader, DeliveryAddressIcon, DeliveryAddressText, DeliveryForm, FinishOrder, OrderDetails, OrderDetailsContainer, OrderSummary, Payment, PaymentHeader, PaymentIcon, PaymentMethods, PaymentText, SelectedProducts, StyledHr, Subtext, SummaryItem, SummaryTotal, Title } from "./styles";
+import { Subtitle } from "../Home/components/Banner/styles";
+import { Input } from "./components/Input";
+import { PaymentMethod } from "./components/PaymentMethod";
+import { SelectedProduct } from "./components/SelectedProduct";
+
+export function Checkout() {
+    return (
+        <Container>
+            <FinishOrder>
+                <Title>Complete seu pedido</Title>
+                <DeliveryAddress>
+                    <DeliveryAddressHeader>
+                        <DeliveryAddressIcon>
+                            <MapPinAreaIcon size={22} />
+                        </DeliveryAddressIcon>
+                        <DeliveryAddressText>
+                            <Subtitle>Endereço de Entrega </Subtitle>
+                            <Subtext>Informe o endereço onde deseja receber seu pedido</Subtext>
+                        </DeliveryAddressText>
+                    </DeliveryAddressHeader>
+
+
+                    <DeliveryForm>
+                        <Input placeholder='CEP' size='S' />
+                        <Input placeholder='Rua' size='L' />
+                        <Input placeholder='Número' size='S' />
+                        <Input placeholder='Complemento' size='M' optional />
+                        <Input placeholder='Bairro' size='S' />
+                        <Input placeholder='Cidade' size='S' />
+                        <Input placeholder='UF' size='XS' />
+                    </DeliveryForm>
+                </DeliveryAddress>
+
+                <Payment>
+                    <PaymentHeader>
+                        <PaymentIcon>
+                            <CurrencyDollarIcon size={22} />
+                        </PaymentIcon>
+
+                        <PaymentText>
+                            <Subtitle>Pagamento </Subtitle>
+                            <Subtext>O pagamento é feito na entrega. Escolha a forma que deseja pagar</Subtext>
+                        </PaymentText>
+                    </PaymentHeader>
+
+                    <PaymentMethods>
+                        <PaymentMethod type={'credit'} />
+                        <PaymentMethod type={'debit'} />
+                        <PaymentMethod type={'cash'} />
+                    </PaymentMethods>
+
+                </Payment>
+
+            </FinishOrder>
+
+            <OrderDetailsContainer>
+                <Title>Cafés selecionados</Title>
+                <OrderDetails>
+                    <SelectedProducts>
+                        <SelectedProduct>
+                        </SelectedProduct>
+                        <StyledHr />
+
+                        <SelectedProduct>
+                        </SelectedProduct>
+                        <StyledHr />
+                    </SelectedProducts>
+                    <OrderSummary>
+                        <SummaryItem>
+                            <span>Total de itens</span>
+                            <span>R$ 29,70</span>
+                        </SummaryItem>
+
+                        <SummaryItem>
+                            <span>Entrega</span>
+                            <span>R$ 3,50</span>
+                        </SummaryItem>
+
+                        <SummaryTotal>
+                            <span>Total</span>
+                            <span>R$ 33,20</span>
+                        </SummaryTotal>
+                        <ConfirmOrderButton>CONFIRMAR PEDIDO</ConfirmOrderButton>
+                    </OrderSummary>
+                </OrderDetails>
+            </OrderDetailsContainer>
+        </Container>
+    )
+}
---
+++ src/pages/Checkout/styles.tsx
@@ -0,0 +1,166 @@
+import styled from "styled-components";
+
+export const Title = styled.h2`
+    font-size: 18px;
+    color: ${props => props.theme['base-subtitle']};
+    font-weight: bold;
+    margin-bottom: 10px;
+`
+
+export const Subtext = styled.h2`
+    font-size: 14px;
+    font-family: 'Roboto';
+    font-weight: normal;
+    color: ${props => props.theme['base-text']};
+`
+
+export const Container = styled.div`
+    margin-top: 50px;
+    display: flex;
+    flex-direction: row;
+    gap: 50px;
+`
+
+export const FinishOrder = styled.div`
+
+`
+
+export const DeliveryAddressHeader = styled.div`
+    display: flex;
+    align-items: flex-start;
+    flex-direction: row;
+    gap: 8px;
+`
+
+export const DeliveryAddress = styled.div`
+    background-color: ${props => props.theme['base-card']};
+    padding: 40px;
+    width: 640px;
+
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    gap: 8px;
+
+    border-radius: 6px;
+    margin-bottom: 14px;
+`
+
+export const DeliveryAddressIcon = styled.div`
+    display: flex;
+    align-items: flex-start;
+    gap: 8px;
+    margin-top: 6px;
+    color: ${props => props.theme['yellow-dark']};
+`
+
+export const DeliveryAddressText = styled.div`
+`
+
+export const DeliveryForm = styled.form`
+    margin-top: 18px;
+`
+
+export const PaymentHeader = styled.div`
+display: flex;
+    align-items: flex-start;
+    flex-direction: row;
+    gap: 8px;
+`
+export const Payment = styled.div`
+    background-color: ${props => props.theme['base-card']};
+    padding: 40px;
+    width: 640px;
+
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    gap: 8px;
+
+    border-radius: 6px;
+    margin-bottom: 14px;
+
+`
+
+export const PaymentIcon = styled.div`
+    display: flex;
+    align-items: flex-start;
+    margin-top: 6px;
+    color: ${props => props.theme['purple']};
+`
+export const PaymentText = styled.div`
+`
+
+export const PaymentMethods = styled.div`
+    margin-top: 18px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    gap: 8px;
+`
+
+export const OrderDetailsContainer = styled.div`
+`
+
+export const OrderDetails = styled.div`
+    background-color: ${props => props.theme['base-card']};
+    width: 448px;
+    min-height: 350px;
+    border-radius: 5px 40px;
+`
+export const SelectedProducts = styled.div`
+    padding: 40px 40px 0px;
+
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+`
+
+export const OrderSummary = styled.div`
+    background-color: ${props => props.theme['base-card']};
+    padding: 40px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 4px;
+    border-radius: 0px 0px 5px 40px;
+`
+
+export const SummaryItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 16px;
+  color: ${props => props.theme['base-text']};
+`;
+
+export const SummaryTotal = styled(SummaryItem)`
+  font-size: 20px;
+  font-weight: bold;
+  color: ${props => props.theme['base-subtitle']};
+`;
+
+export const ConfirmOrderButton = styled.div`
+    background-color: ${props => props.theme['yellow']};
+    color: ${props => props.theme['white']};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    padding: 8px 12px;
+    border-radius: 6px;
+
+    font-weight: bold;
+
+    margin-top: 14px;
+
+    cursor: pointer;
+`
+
+export const StyledHr = styled.hr`
+    border: none;
+    margin: 0;
+    padding: 0;
+    height: 1px;
+    background-color: ${props => props.theme['base-button']}; 
+`
