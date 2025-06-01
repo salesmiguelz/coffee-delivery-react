70ef4cdf48f1406d0852f4b73a3970868cf88098
salesmiguelz <miguelsales02@gmail.com>
2025-06-01 11:34:21 -0300

    feat: checkout on router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { DefaultLayout } from "./layouts/DefaultLayout";
 import { Home } from "./pages/Home";
+import { Checkout } from "./pages/Checkout";
 
 
 export function Router() {
@@ -8,6 +9,7 @@ export function Router() {
         <Routes>
             <Route path="/" element={<DefaultLayout />}>
                 <Route path="/" element={<Home />} />
+                <Route path="/checkout" element={<Checkout />} />
             </Route>
         </Routes>
     )
