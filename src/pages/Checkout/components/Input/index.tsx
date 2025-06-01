bd13d7302cff2975b9a5b90ddeef4e641daa7e47
salesmiguelz <miguelsales02@gmail.com>
2025-06-01 11:04:58 -0300

    feat: input components

--- src/pages/Checkout/components/Input/index.tsx
+++ src/pages/Checkout/components/Input/index.tsx
@@ -1,7 +1,17 @@
-import { StyledInput } from "./styles";
+import { Container, OptionalLabel, StyledInput } from "./styles";
 
-export function Input() {
+interface InputProps {
+    size: 'XS' | 'S' | 'M' | 'L',
+    placeholder: string,
+    optional?: boolean
+}
+
+export function Input({ size, placeholder, optional }: InputProps) {
     return (
-        <StyledInput size="L" />
+        <Container>
+            <StyledInput size={size} placeholder={placeholder} optional={optional} />
+            {optional && <OptionalLabel>Opcional</OptionalLabel>}
+        </Container>
+
     )
 }
--- src/pages/Checkout/components/Input/styles.tsx
+++ src/pages/Checkout/components/Input/styles.tsx
@@ -1,15 +1,21 @@
 import styled from "styled-components";
 
 const InputSize = {
-    'P': '100px',
-    'M': '200px',
-    'L': '300px',
-    'XL': '400px',
+    'XS': '138px',
+    'S': '200px',
+    'M': '348px',
+    'L': '560px',
 }
 
 interface InputProps {
-    size: 'P' | 'M' | 'L' | 'XL'
+    size: 'XS' | 'S' | 'M' | 'L',
+    optional?: boolean
 }
+
+export const Container = styled.div`
+  position: relative;
+  display: inline-block;
+`
 
 export const StyledInput = styled.input<InputProps>`
     font-size: 14px;
@@ -17,11 +23,30 @@ export const StyledInput = styled.input<InputProps>`
     color: ${props => props.theme["base-label"]};
     border-radius: 4px;
     padding: 12px;
-    border: none;
     background-color: ${props => props.theme["base-input"]};
     width: ${props => InputSize[props.size]};
+    height: 42px;
+
+    margin: 8px 5px;
+
+    border: none;
+    outline: 1px solid ${props => props.theme['base-button']}; ;
+    padding-right: 60px;
+
 
     &:focus{
         outline: 1px solid ${props => props.theme['yellow-dark']};
     };
+`
+
+export const OptionalLabel = styled.span`
+  position: absolute;
+  right: 12px;
+  top: 50%;
+  transform: translateY(-50%);
+  font-size: 12px;
+  font-style: italic;
+  color: ${props => props.theme['base-label']};
+  pointer-events: none;
+  font-family: 'Roboto';
 `
