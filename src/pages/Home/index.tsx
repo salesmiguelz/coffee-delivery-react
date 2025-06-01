329b85389474ac6997cc587a8ffec08c3d3858ae
salesmiguelz <miguelsales02@gmail.com>
2025-06-01 11:35:04 -0300

    feat: home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Banner } from "./components/Banner";
+import { Coffees } from "./components/Coffees";
+
+export function Home() {
+    return (
+        <>
+            <Banner />
+            <Coffees />
+        </>
+    )
+}
